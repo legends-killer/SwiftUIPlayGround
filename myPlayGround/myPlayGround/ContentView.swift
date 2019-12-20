627bf65614a1f3a989c32187dbf671437151c9fd
legends-killer <958775729@qq.com>
2019-12-21 01:29:48 +0800

    create playground

---
+++ myPlayGround/myPlayGround/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  myPlayGround
+//
+//  Created by ljz on 2019/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
